2384d434da02c5daa2cca5e1744743eaf358afc5
supoj <supoj.moon@gmail.com>
2018-03-21 14:48:57 +0700

    Create Toolbar Success

--- app/src/main/java/supoj/th/ac/ms/supojreadcode/fragment/MainFragment.java
+++ app/src/main/java/supoj/th/ac/ms/supojreadcode/fragment/MainFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import supoj.th.ac.ms.supojreadcode.R;
 
@@ -14,6 +15,32 @@ import supoj.th.ac.ms.supojreadcode.R;
  */
 
 public class MainFragment extends Fragment{
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+       // Register Controller
+        TextView textView = getView().findViewById(R.id.txtRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+              //  Replace Fragment
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contentMainFragment, new RegisterFragment())
+                        .addToBackStack(null)
+                        .commit();
+
+
+            }
+        });
+
+
+
+    }  ///Main Method
 
     @Nullable
     @Override
--- app/src/main/java/supoj/th/ac/ms/supojreadcode/fragment/RegisterFragment.java
+++ app/src/main/java/supoj/th/ac/ms/supojreadcode/fragment/RegisterFragment.java
@@ -3,10 +3,12 @@ package supoj.th.ac.ms.supojreadcode.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import supoj.th.ac.ms.supojreadcode.MainActivity;
 import supoj.th.ac.ms.supojreadcode.R;
 
 /**
@@ -14,6 +16,31 @@ import supoj.th.ac.ms.supojreadcode.R;
  */
 
 public class RegisterFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // Create Toolbar
+
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
+        //Setup Title
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle("Register");
+
+     //   show navigator bar
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+    } //Main Method
 
     @Nullable
     @Override
